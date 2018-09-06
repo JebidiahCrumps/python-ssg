8bcd7f88b8ce7001bf16fcc41392702f40b10c85
JebidiahCrumps <31321378+JebidiahCrumps@users.noreply.github.com>
2018-09-06 19:53:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import markdown
 
-def mdConvert(
+def md2html():
 
-#make sure to have "import markdown" at the top
-#This block of code will ask for a file name
-#then open that file read it and turn
-#it into HTML with a default css theme
+    #make sure to have "import markdown" at the top
+    #This block of code will ask for a file name
+    #then open that file read it and turn
+    #it into HTML with a default css theme
 
-print("Enter the markdown filename: ")
-fileName = input()
-input_file = open(fileName)
-text = input_file.read()
-html = markdown.markdown(text)
-output_file = open("TheHTML.html", "w")
-output_file.write(html)
-)
+    print("Enter the markdown filename: ")
+    fileName = input()
+    input_file = open(fileName)
+    text = input_file.read()
+    html = markdown.markdown(text)
+    output_file = open("TheHTML.html", "w")
+    output_file.write(html)
+
+md2html()
