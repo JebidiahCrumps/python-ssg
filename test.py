33c20a84aeeeb854b368edd97defdaaabf2bbc7d
JebidiahCrumps <31321378+JebidiahCrumps@users.noreply.github.com>
2018-09-06 19:44:57 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import markdown
+
+def mdConvert(
+
+#make sure to have "import markdown" at the top
+#This block of code will ask for a file name
+#then open that file read it and turn
+#it into HTML with a default css theme
+
+print("Enter the markdown filename: ")
+fileName = input()
+input_file = open(fileName)
+text = input_file.read()
+html = markdown.markdown(text)
+output_file = open("TheHTML.html", "w")
+output_file.write(html)
+)
