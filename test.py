a655cfeaffe48df1d0556e4b779e8470459c4d91
JebidiahCrumps <31321378+JebidiahCrumps@users.noreply.github.com>
2018-09-06 20:06:57 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,10 +9,23 @@ def md2html():
 
     print("Enter the markdown filename: ")
     fileName = input()
+
+    try:
+        with open(fileName) as file:
+            pass
+    except IOError as e:
+        print("Unable to open file")  # Does not exist OR no read permissions
+        exit()
     input_file = open(fileName)
+
     text = input_file.read()
+
     html = markdown.markdown(text)
+
     output_file = open("TheHTML.html", "w")
+
     output_file.write(html)
+
+    print("Converted!")
 
 md2html()
